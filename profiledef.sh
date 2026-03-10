bfc0b945c8206e4026d426aa977a0f663e73d8a1
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2026-03-10 14:03:38 +0200

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="Helwan-Linux-Dev-LTS"
-iso_label="Helwan-Linux-Dev-LTS-v3.1"
+iso_label="Helwan-Linux-Dev-LTS-v4.0"
 iso_publisher="helwanlinux <helwanlinux@gmail.com>"
 iso_application="Helwan Linux Live/Rescue DVD"
-iso_version="v3.1"
+iso_version="v4.0"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
